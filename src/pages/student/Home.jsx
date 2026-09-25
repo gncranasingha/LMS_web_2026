521647c09013b178f5652c8fea6a10424ab2a7b8
gncranasingha <chanakanipun10@gmail.com>
2026-09-25 10:52:53 +0530

    feat:create footer

--- src/pages/student/Home.jsx
+++ src/pages/student/Home.jsx
@@ -1,6 +1,7 @@
 import CallToAction from "../../components/student/CallToAction"
 import Companies from "../../components/student/Companies"
 import CoursesSection from "../../components/student/CoursesSection"
+import Footer from "../../components/student/Footer"
 import Hero from "../../components/student/Hero"
 import TstimonialsSection from "../../components/student/TstimonialsSection"
 
@@ -12,6 +13,7 @@ const Home = () => {
       <CoursesSection/>
       <TstimonialsSection/>
       <CallToAction/>
+      <Footer/>
     </div>
   )
 }
